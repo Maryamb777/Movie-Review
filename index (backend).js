e7afaf4c54426a9b7c598114b350ee92fb855ea8
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2024-01-01 13:52:15 +0500

    mongoclient

--- index (backend).js
+++ index (backend).js
@@ -7,6 +7,7 @@ const mongo_username = process.env.maryamsocial777
 const mongo_password = process.env.MONGO_PASSWORD
 const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.u4gnm58.mongodb.net/?retryWrites=true&w=majority`
 
+// specify port
 const port = 8000
 
 MongoClient.connect(

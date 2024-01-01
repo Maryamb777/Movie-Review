129ec820b931c8790fac3a3cdc4a055a0b3fb51b
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2024-01-01 18:31:48 +0500

    comment on index

--- index (backend).js
+++ index (backend).js
@@ -7,7 +7,7 @@ const mongo_username = process.env.maryamsocial777
 const mongo_password = process.env["6GcvDZ3sojXllQrQ"];
 const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.u4gnm58.mongodb.net/?retryWrites=true&w=majority`
 
-// specify port
+// specify port, check how to curl later via -x post -H "Content-Type: application/json" -d '{"name": "Maryam", "review": "I love this movie"}' http://localhost:5501/api/v1/reviews
 const port = 5501
 
 MongoClient.connect(
